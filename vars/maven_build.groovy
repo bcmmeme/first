dfca85ababa658fb316427ee750cdba59a014cb5
bcmmeme <68555262+bcmmeme@users.noreply.github.com>
2020-07-27 16:34:24 +0900

    Update maven_build.groovy

--- vars/maven_build.groovy
+++ vars/maven_build.groovy
@@ -1,1 +1,24 @@
-def call()
+def call(){
+  node {
+    checkout scm
+    
+    def testType
+    stage('input'){
+      def userInput = input message: 'Choose test type', parameters: [string]
+      testType = userInput.testType
+    }
+    
+    stage('build') {
+      withEnv["PATH+MAVEN=$tool 'mvn-3.6.0'}/bin"]) {
+        sh 'mvn --version'
+        sh "mvn clean $(testType)"
+      }
+    }
+    
+    stage('report') {
+      junit 'target/surefire-reports/*.xml'
+      jacoco exePattern: 'target/**.exec'
+      addShortText background: '', borderColor: '', color: ''
+    }
+  }
+}
